2473c464c0021c5d078e8f5bd5c55034e1298d16
vunv1351 <vunv1351@gmail.com>
2018-05-25 09:52:25 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 call echo "build"
-call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" "C:\Users\Administrator\Desktop\RabbitMQ.sln"
+call "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe" "C:\Users\Administrator\Desktop\RabbitMQ\RabbitMQ.sln"
 pause
